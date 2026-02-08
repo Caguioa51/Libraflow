8ac5babedbfa42d0d3a11f239c62d089ed72f6ce
Caguioa51 <markcaguioa747@gmail.com>
2026-02-09 05:30:27 +0800

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,12 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends Factory<User>
- */
 class UserFactory extends Factory
 {
-    /** @var class-string<\App\Models\User> */
-    protected $model = User::class;
-
     protected static ?string $password;
 
     public function definition(): array
