54c1875e6cf72e4cf5cfba6ef8e825d08b0de801
Caguioa51 <markcaguioa747@gmail.com>
2026-02-09 05:35:00 +0800

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->name(),               // <- use $this->faker
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
